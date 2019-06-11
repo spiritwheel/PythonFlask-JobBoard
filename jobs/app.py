eb0099924996f8343892575740ba51560ccd4df9
FERNANDEZ <fernandez.logan@yahoo.fr>
2019-06-11 12:05:43 +0200

    sqlite3 Row Factory
    To make accessing data easier, after the if statement in open_connection:
    
        Set the row_factory of connection to sqlite3.Row. Note: All rows returned from the database will be named tuples.
        Return the connection variable.

--- jobs/app.py
+++ jobs/app.py
@@ -5,8 +5,13 @@ PATH = 'db/jobs.sqlite'
 
 def open_connection():
     connection = getattr(g, '_connection', None)
+    
     if connection == None:
         connection = g._connection = sqlite3.connect(PATH)
+
+    connection.row_factory = sqlite3.Row
+
+    return connection
 
 app = Flask(__name__)
 
