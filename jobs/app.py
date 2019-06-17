b08e48c17c1ff496c6e1cf1cb72790f47002efe6
FERNANDEZ <fernandez.logan@yahoo.fr>
2019-06-17 10:15:02 +0200

    4. Insert Review
    Still in the review function, in the if statement below the date variable, call the execute_sql function with the following arguments:
    
        'INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)'
        (review, rating, title, date, status, employer_id)
        commit=True

--- jobs/app.py
+++ jobs/app.py
@@ -56,5 +56,6 @@ def review(employer_id):
         status = request.form['status']
 
         date = datetime.datetime.now().strftime("%m/%d/%Y")
+        execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)',(review, rating, title, date, status, employer_id), commit=True)
     return render_template('review.html',employer_id=employer_id)
 
